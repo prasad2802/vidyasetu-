b6b6e350f2a014302126910c6de591d66c71f03b
prasad2802 <140104152+prasad2802@users.noreply.github.com>
2025-09-07 22:19:14 +0530

    Update app_groq_practice.py

--- app_groq_practice.py
+++ app_groq_practice.py
@@ -1,7 +1,8 @@
 import os, time, random, requests
+os.environ["GRADIO_ROOT_PATH"] = "/tutor"
 import gradio as gr
 import sympy as sp
-
+from app_groq_practice import demo
 # =========================
 # Settings
 # =========================
@@ -321,3 +322,4 @@ with gr.Blocks() as demo:
         choice = gr.Dropdown(choices=["A","B","C","D"], label="Your Answer")
         check = gr.Button("Check Answer")
         check.click(check_answer, [choice], [out1, out2, out3])
+
